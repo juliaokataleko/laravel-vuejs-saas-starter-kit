c4c9e7628b09ddc6bddbdfee3c83e4055224070d
Julião Kataleko <juliofeli78@gmail.com>
2024-06-23 20:41:49 +0100

    navbar

--- resources/js/Components/SidebarLink.vue
+++ resources/js/Components/SidebarLink.vue
@@ -14,7 +14,7 @@ const props = defineProps({
 
 const classes = computed(() =>
     props.active
-        ? 'bg-zinc-700 rounded-xl shadow text-sm font-medium leading-5 text-white  focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+        ? 'bg-slate-700 rounded-xl shadow text-sm font-medium leading-5 text-white  focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
         : 'text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out'
 );
 </script>
--- resources/js/Layouts/SidebarLayout.vue
+++ resources/js/Layouts/SidebarLayout.vue
@@ -15,9 +15,9 @@ const showingNavigationDropdown = ref(false);
     <div>
         <div class="min-h-screen flex bg-gray-100 flex dark:bg-gray-900">
             <!-- Start Sidebar -->
-            <div class="h-screen bg-zinc-800 w-[300px]">
+            <div class="h-screen bg-slate-800 w-[300px]">
 
-                <div class="h-16 shadow shadow-zinc-700 flex items-center px-4">
+                <div class="h-16 shadow shadow-slate-700 flex items-center px-4">
                     <Link :href="route('dashboard')" class=" flex items-center space-x-3">
                         <ApplicationLogo
                             class="block h-9 w-auto fill-current text-gray-300 dark:text-gray-200"
@@ -193,9 +193,39 @@ const showingNavigationDropdown = ref(false);
             <div
                 class="h-screen overflow-y-auto w-[calc(100vw-300px)] bg-zinc-50"
             >
-                <div class="h-16 bg-white shadow-md"></div>
-                <div class="p-5">
+                <div class=" bg-white shadow-md">
+                    <div class="px-5 max-w-[1300px] mx-auto h-16 flex justify-between items-center">
+                        <header class="" v-if="$slots.header">
+                            <div class="">
+                                <slot name="header" />
+                            </div>
+                        </header>
+
+                        <div class=" flex items-center space-x-2">
+                            <div class="border p-1 px-2 bg-slate-50 rounded-lg flex items-center">
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                                    </svg>
+                                </span>
+                                <input type="text" class=" border-none p-1 bg-transparent" placeholder="Search" />
+                            </div>
+
+                            <button class="border border-slate-300 p-2 rounded-lg">
+
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                                </svg>
+
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="px-5 max-w-[1300px] mx-auto py-8">
+
                     <slot />
+                    </div>
                 </div>
             </div>
             <!-- End Main Content -->
