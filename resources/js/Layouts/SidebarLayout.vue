dfc2ee4e26eebd13f86a1073d81b7fb8fb000adb
Julião Kataleko <juliofeli78@gmail.com>
2024-06-24 10:03:53 +0100

    sidebar toggle

--- resources/js/Layouts/SidebarLayout.vue
+++ resources/js/Layouts/SidebarLayout.vue
@@ -7,15 +7,25 @@ import NavLink from "@/Components/NavLink.vue";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
 import { Link } from "@inertiajs/vue3";
 import SidebarLink from "@/Components/SidebarLink.vue";
+import { onMounted } from "vue";
 
-const showingNavigationDropdown = ref(false);
+const showingSidemenuToogle = ref(true);
+const sidemenuToogle = () => {
+    showingSidemenuToogle.value = !showingSidemenuToogle.value
+}
+
+onMounted(() => {
+    const screenWidth = window.innerWidth
+    if (screenWidth < 705) showingSidemenuToogle.value = false
+})
+
 </script>
 
 <template>
     <div>
         <div class="min-h-screen flex bg-gray-100 flex dark:bg-gray-900">
             <!-- Start Sidebar -->
-            <div class="h-screen bg-slate-800 w-[300px]">
+            <div class="h-screen bg-slate-800 w-[300px] fixed md:relative top-0 left-0 bottom-0" :class="{'hidden': !showingSidemenuToogle}">
 
                 <div class="h-16 shadow shadow-slate-700 flex items-center px-4">
                     <Link :href="route('dashboard')" class=" flex items-center space-x-3">
@@ -191,7 +201,8 @@ const showingNavigationDropdown = ref(false);
 
             <!-- Main Content -->
             <div
-                class="h-screen overflow-y-auto w-[calc(100vw-300px)] bg-zinc-50"
+                class="h-screen overflow-y-auto  bg-zinc-50"
+                :class="{'w-full md:w-[calc(100vw-300px)] ': showingSidemenuToogle, 'w-full': !showingSidemenuToogle}"
             >
                 <div class=" bg-white shadow-md">
                     <div class="px-5 max-w-[1300px] mx-auto h-16 flex justify-between items-center">
@@ -216,7 +227,7 @@ const showingNavigationDropdown = ref(false);
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                                 </svg>
                             </button>
-                            <button class="border border-slate-300 p-2 rounded-lg">
+                            <button class="border border-slate-300 p-2 rounded-lg" @click="sidemenuToogle()">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
                                 </svg>
